22f560fb4e7bdbd905962b23007d96c83443a537
Geverson <geeh-eb@hotmail.com>
2021-02-05 03:33:49 -0300

    routes: create route to finish task

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router, Response, Request } from "express";
-import { indexTasks, storeTasks, showTask, updateTasks }  from '../src/controller/TasksController'; 
+import { indexTasks, storeTasks, showTask, updateTasks, finishedTask }  from '../src/controller/TasksController'; 
 const routes = Router();
 
 routes.post('/task/store', storeTasks);
 routes.get('/task/:id', showTask);
 routes.put('/task/updated/:id', updateTasks);
 routes.get('/tasks/index', indexTasks);
+routes.patch('/tasks/:id', finishedTask);
 
 export default routes;
