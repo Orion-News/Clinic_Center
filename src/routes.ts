9a6a59a5339cbe38a643518960010f436851be81
Geverson <geeh-eb@hotmail.com>
2021-02-05 03:19:11 -0300

    routes: create new routes to updated and show task

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router, Response, Request } from "express";
-import { indexTasks, storeTasks }  from '../src/controller/TasksController'; 
+import { indexTasks, storeTasks, showTask, updateTasks }  from '../src/controller/TasksController'; 
 const routes = Router();
 
-routes.get('/index', indexTasks);
-routes.post('/store', storeTasks);
+routes.post('/task/store', storeTasks);
+routes.get('/task/:id', showTask);
+routes.put('/task/updated/:id', updateTasks);
+routes.get('/tasks/index', indexTasks);
 
 export default routes;
