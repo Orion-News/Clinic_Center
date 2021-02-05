03a620dbf51163229da4c0f31a9cce4a92796487
Geverson <geeh-eb@hotmail.com>
2021-02-05 05:32:50 -0300

    routes: created router to delte task to id

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,11 @@ import { Router, Response, Request } from "express";
 import TasksController  from '../src/controller/TasksController'; 
 const routes = Router();
 
-// routes.post('/task/store', storeTasks);
-// routes.get('/task/:id', showTask);
-// routes.put('/task/updated/:id', updateTasks);
-routes.get('/tasks/index', TasksController.index);
-// routes.patch('/tasks/:id', finishedTask);
+routes.post('/task/store', TasksController.store);
+routes.get('/task/:id', TasksController.show);
+routes.put('/task/:id', TasksController.update);
+routes.delete('/task/:id')
+routes.get('/task/index', TasksController.index);
+routes.patch('/task/:id', TasksController.finishTask);
 
 export default routes;
