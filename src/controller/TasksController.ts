106d90d007481ab1e3dc3aa96c62515c4b10e7a4
Geverson <geeh-eb@hotmail.com>
2021-02-05 03:34:25 -0300

    tasks-Controller: created new method to finish task for id

--- src/controller/TasksController.ts
+++ src/controller/TasksController.ts
@@ -79,4 +79,29 @@ export const indexTasks = async (request: Request, response: Response) => {
     }
 };
 
+export const finishedTask = async (request: Request, response: Response) => {
+    try {
+        const { id } = request.params;
+        const task = await getRepository(Tasks).update(id,{ 
+            finished: true
+        });
+
+        if(task.affected === 1) {
+            const taskUpdated = await getRepository(Tasks).findOne(id);
+            const result = {
+                status: 200,
+                message: 'Successful, your task Finish!',
+                data: taskUpdated
+            }
+            return response.json(result);
+        }
+
+        return response.status(404).json({"Mensage": "Task not Found!"});
+
+    } catch (e) {
+        return response.status(500).json({"Menssage": `${e}`})
+    }
+
+};
+
 
