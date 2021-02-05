89b79942e41a4a9dbf6c2e48f8ed76670c386036
Geverson <geeh-eb@hotmail.com>
2021-02-05 05:30:30 -0300

    tasksController: changed arrow function to method

--- src/controller/TasksController.ts
+++ src/controller/TasksController.ts
@@ -80,66 +80,42 @@ class TasksController {
             return response.status(500).json({"Menssage": `${e}`})
         }
     };
+
+    public async delete (request: Request, response: Response): Promise<Response> {
+        try {
+            const { id } = request.params;
+            const task = await getRepository(Tasks).delete(id);
+    
+            return response.json( task ? "Task delete with success" : "Task not Removed");
+        } catch (e) {
+            return response.status(404).json({"Menssage" : `${e}`});
+        }
+    };
+
+    public async finishTask (request: Request, response: Response): Promise<Response> {
+        try {
+            const { id } = request.params;
+            const task = await getRepository(Tasks).update(id,{ 
+                finished: true
+            });
+    
+            if(task.affected === 1) {
+                const taskUpdated = await getRepository(Tasks).findOne(id);
+                const result = {
+                    status: 200,
+                    message: 'Successful, your task Finish!',
+                    data: taskUpdated
+                }
+                return response.json(result);
+            }
+    
+            return response.status(404).json({"Mensage": "Task not Found!"});
+    
+        } catch (e) {
+            return response.status(500).json({"Menssage": `${e}`})
+        }
+    
+    };
+
 }
 export default new TasksController();
-
-// export const storeTasks = 
-
-// export const showTask = 
-
-// export const updateTasks = 
-
-// export const indexTasks = async (request: Request, response: Response) => {
-//     try {
-//         const tasks = await getRepository(Tasks).find();
-        
-//         if (!tasks) throw new Error(`Deu Xabu, Não tem tasks!`);
-
-//         const result = {
-//             status: 200,
-//             message: 'Successful, find all Tasks',
-//             data: tasks
-//         };
-        
-//         return response.json(result);
-    
-//     } catch (e) {
-//         return response.status(500).json({"Menssage": `${e}`})
-//     }
-// };
-
-// export const finishedTask = async (request: Request, response: Response) => {
-//     try {
-//         const { id } = request.params;
-//         const task = await getRepository(Tasks).update(id,{ 
-//             finished: true
-//         });
-
-//         if(task.affected === 1) {
-//             const taskUpdated = await getRepository(Tasks).findOne(id);
-//             const result = {
-//                 status: 200,
-//                 message: 'Successful, your task Finish!',
-//                 data: taskUpdated
-//             }
-//             return response.json(result);
-//         }
-
-//         return response.status(404).json({"Mensage": "Task not Found!"});
-
-//     } catch (e) {
-//         return response.status(500).json({"Menssage": `${e}`})
-//     }
-
-// };
-
-// export const deleteTask = async (request: Request, response: Response) => {
-//     try {
-//         const { id } = request.params;
-//         const task = await getRepository(Tasks).delete(id);
-
-//         return response.json( task ? "Task delete with success" : "Task not Removed");
-//     } catch (e) {
-//         return response.status(404).json({"Menssage" : `${e}`});
-//     }
-// }
