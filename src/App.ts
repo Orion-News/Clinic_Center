2e6fe5161581277e9010aca013ff5f422317967a
Geverson <geeh-eb@hotmail.com>
2021-02-05 05:23:06 -0300

    App: modified to class app

---
+++ src/App.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import { createConnection } from 'typeorm';
+import routes from "./routes";
+import * as express from 'express';
+import * as cors from 'cors';
+
+
+class App {
+    public express: express.Application;
+
+    public constructor () {
+        this.express = express();
+        this.routes();
+        this.middlewares();
+        this.dataBase();
+    }
+
+    private middlewares (): void {
+        this.express.use(express.json());
+        this.express.use(cors());
+    }
+
+    private dataBase (): void {
+        createConnection();
+    }
+
+    private routes (): void {
+        this.express.use(routes);
+    }
+}
+
+export default new App().express;
