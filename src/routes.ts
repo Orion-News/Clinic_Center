a495290f948698cbfcd4fce99ffdb0337596daf2
Geverson <geeh-eb@hotmail.com>
2021-02-05 05:22:00 -0300

    changed method of class

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Router, Response, Request } from "express";
-import { indexTasks, storeTasks, showTask, updateTasks, finishedTask }  from '../src/controller/TasksController'; 
+import TasksController  from '../src/controller/TasksController'; 
 const routes = Router();
 
-routes.post('/task/store', storeTasks);
-routes.get('/task/:id', showTask);
-routes.put('/task/updated/:id', updateTasks);
-routes.get('/tasks/index', indexTasks);
-routes.patch('/tasks/:id', finishedTask);
+// routes.post('/task/store', storeTasks);
+// routes.get('/task/:id', showTask);
+// routes.put('/task/updated/:id', updateTasks);
+routes.get('/tasks/index', TasksController.index);
+// routes.patch('/tasks/:id', finishedTask);
 
 export default routes;
