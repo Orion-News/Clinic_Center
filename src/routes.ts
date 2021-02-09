25bab64a30854597e31c341d8abb3ec05d764157
Geverson <geeh-eb@hotmail.com>
2021-02-08 22:38:27 -0300

    routes: create router to user singIN

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,19 @@
 import { json, Router } from "express";
 import { storeTasks, indexTasks, showTask, deleteTask, finishedTask, updateTasks } from '../src/controller/TasksController'; 
+import { storeUser } from '../src/controller/UserController'; 
 const routes = Router();
 
 routes.use(json());
 
+// routes to tasks
 routes.post('/task/store', storeTasks);
 routes.get('/task/:id', showTask);
 routes.put('/task/:id', updateTasks);
 routes.delete('/task/:id', deleteTask);
 routes.get('/task', indexTasks);
 routes.patch('/task/:id', finishedTask);
+
+// routes to user
+routes.post('/signIn', storeUser);
 
 export default routes;
