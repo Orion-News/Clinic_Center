d4eda59a2752009395f36e2c562507b58c2fc2d6
Geverson <geeh-eb@hotmail.com>
2021-02-10 09:00:44 -0300

    routes: import middleware

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { json, Router } from "express";
 import { storeTasks, indexTasks, showTask, deleteTask, finishedTask, updateTasks } from '../src/controller/TasksController'; 
 import { storeUser } from '../src/controller/UserController'; 
 import { signIn } from '../src/controller/AuthController'; 
+import { authenticate } from '../src/middlewares/auth';
 const routes = Router();
 
 routes.use(json());
@@ -11,11 +12,11 @@ routes.post('/signIn', signIn);
 
 
 // routes to tasks
-routes.post('/task/store', storeTasks);
+routes.post('/task/store', authenticate, storeTasks);
 routes.get('/task/:id', showTask);
 routes.put('/task/:id', updateTasks);
 routes.delete('/task/:id', deleteTask);
-routes.get('/task', indexTasks);
+routes.get('/task', authenticate, indexTasks);
 routes.patch('/task/:id', finishedTask);
 
 
