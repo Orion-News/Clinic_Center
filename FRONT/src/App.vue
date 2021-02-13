880a5c2aa0dc332cc98110dfe49cfd042545df07
Geverson <geeh-eb@hotmail.com>
2021-02-13 14:53:56 -0300

    App: removed itens

--- FRONT/src/App.vue
+++ FRONT/src/App.vue
@@ -1,30 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
-    <router-view/>
+    <h4> Clinic Center </h4>
+    <img src="./assets/assets/logo.png">
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  img {
+    width: 150;
+    height: 100px
   }
-}
 </style>
